e345061926ff51dfa833a200a585652e2d6dd382
thomas9120 <pegasus.sgc+github@gmail.com>
2026-04-28 14:48:15 -0400

    update

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
 @echo off
 cd /d "%~dp0"
+echo Character Card Manager running at:
+echo http://localhost:8000/index.html
+echo.
+start "" http://localhost:8000/index.html
 python -m http.server 8000
